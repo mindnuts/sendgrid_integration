42dc48fa94d7c13b8d8fdae37d9e9b3ae7312af7
Alex Melkoff <alex.melkoff@gmail.com>
2016-03-07 17:27:19 +0800

    License headers updated

--- sendgrid_integration/blacklist.py
+++ sendgrid_integration/blacklist.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2016, Semilimes Ltd.
+# Copyright (c) 2016, Semilimes
 # For license information, please see license.txt
 
 import frappe
--- sendgrid_integration/webhook_events.py
+++ sendgrid_integration/webhook_events.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2016, Semilimes Ltd.
+# Copyright (c) 2016, Semilimes
 # MIT License. See license.txt
 
 import json
