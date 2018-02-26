6b933a269acbe07d91d89f7024d05cd277ec3c50
Suyash Phadtare <suyashphadtare93@gmail.com>
2018-02-26 09:47:23 +0530

    First Commit

--- sendgrid_integration/sendgrid_integration/email_account.js
+++ sendgrid_integration/sendgrid_integration/email_account.js
@@ -1,12 +1,3 @@
-$.extend(email_defaults, {
-	"SendGrid": {
-		"enable_outgoing": 1,
-		"smtp_server": "smtp.sendgrid.net",
-		"smtp_port": 587,
-		"use_tls": 1
-	}
-});
-
 frappe.ui.form.on("Email Account", {
 	refresh: function(frm) {
 		frm.dashboard.reset();
